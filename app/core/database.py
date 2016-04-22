c58afa302503a295f352c1dcb235c5811da4830f
Nelson Tran <nelson@nelsontran.com>
2016-04-21 18:42:52 -0700

    Fix import statement in db_init()

--- app/core/database.py
+++ app/core/database.py
@@ -30,5 +30,5 @@ def init_db():
     # Import all modules here that might define models so that
     # they will be registered properly on the metadata. Otherwise
     # you will have to import them first before calling init_db().
-    import core.models
+    import app.core.models
     Base.metadata.create_all(bind=engine)
