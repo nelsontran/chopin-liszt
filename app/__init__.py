33e82025a06daf3f4dd24d902122ca8847ab8187
Nelson Tran <nelson@nelsontran.com>
2016-05-13 18:44:51 -0700

    Move 3rd party dependencies to their own directory

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,22 @@
 #!/usr/bin/env python3
 
 from flask import Flask, render_template
+from flask.ext.login import LoginManager
 from app.database import db_session
+from app.models import User
 
 app = Flask(__name__)
 
+login_manager = LoginManager()
+login_manager.init_app(app)
+
 @app.route("/")
-def landing():
-    return render_template("landing.html")
+def index():
+    return render_template("index.html")
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.get(user_id)
 
 @app.teardown_appcontext
 def shutdown_session(exception=None):
