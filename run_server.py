4de94913f4b119540b7c7677678058bad41c9ceb
Nelson Tran <nelson@nelsontran.com>
2016-05-23 16:00:09 -0700

    Configure Flask to listen on all public IPs
    I am currently developing on an Arch Linux VM. In order to access
    the Flask server from the host machine, the Flask server needs
    to externally visible.

--- run_server.py
+++ run_server.py
@@ -3,4 +3,4 @@
 if __name__ == "__main__":
     from app import app as application
     application.secret_key = 'DEVELOPMENT'
-    application.run(debug=True)
+    application.run(host='0.0.0.0', debug=True)
